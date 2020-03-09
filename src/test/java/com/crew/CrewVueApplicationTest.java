776379fcc6ce4455987a1bf3c30fe20ea33896ec
JaeHan-Kim <hgal8877@gmail.com>
2020-03-09 14:48:43 +0900

    test class commit

---
+++ src/test/java/com/crew/CrewVueApplicationTest.java
@@ -0,0 +1,15 @@
+package com.crew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class CrewVueApplicationTest {
+    @SpringBootTest
+    class DemoApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+}
