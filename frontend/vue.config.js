4990c1cf4c46526b4c7d10bf06e237c8acdc73c8
JaeHan-Kim <hgal8877@gmail.com>
2020-05-07 16:59:37 +0900

    axios, vuetify 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,20 +1,20 @@
-'use strict'
-
 module.exports = {
-    devServer: {
-        proxy: { // proxyTable 설정
-            '/api': {
-                target: 'http://localhost:8080',
-                secure: false,
-                changeOrigin: true,
-                publicPath: {
-                    '^/api': '/'
-                }
-            }
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        secure: false,
+        changeOrigin: true,
+        publicPath: {
+          '^/api': '/'
         }
-    },
-
-    outputDir: '../src/main/resources/static',
-    indexPath: "../static/index.html",
-    //assetsDir: '../static'
+      }
+    }
+  },
+  outputDir: '../src/main/resources/static',
+  indexPath: '../static/index.html',
+  //assetsDir: '../static'
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
